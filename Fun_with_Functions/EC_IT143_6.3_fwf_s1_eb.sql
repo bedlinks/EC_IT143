71fe5083bc2a5535894d840dd0bd57b4fbb2ffa0
Ernest Baah Dwomoh <bedlinks@gmail.com>
2025-04-10 00:29:17 +0000

    Adding deliverable 6.3.

---
+++ Fun_with_Functions/EC_IT143_6.3_fwf_s1_eb.sql
@@ -0,0 +1,6 @@
+-- Ernest Baah Dwomoh
+-- EC_IT143 – 6.3 – Fun with Functions – Step 1
+-- Question: How do I extract the first name from a contact name?
+-- This question begins the scalar function journey.
+
+-- No code yet – just a thought-provoking question!
