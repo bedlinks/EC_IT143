876d6277e7718acb8a0610851e778a522f305315
Ernest Baah Dwomoh <bedlinks@gmail.com>
2025-04-10 01:06:15 +0000

    Delete EC_IT143_6.3_fwf_s8_eb.sql

--- EC_IT143_6.3_fwf_s8_eb.sql
+++
@@ -1,9 +0,0 @@
--- Step 8 – Ask next question: How do I extract the last name?
-
--- Then repeat steps 1–7 for:
-CREATE FUNCTION dbo.fn_GetLastName (@ContactName NVARCHAR(100))
-RETURNS NVARCHAR(50)
-AS
-BEGIN
-    RETURN RIGHT(@ContactName, CHARINDEX(' ', REVERSE(@ContactName + ' ')) - 1)
-END;
