6f1e33ba712b18cedc88f6fb2f982dd027fc2794
Ernest Baah Dwomoh <bedlinks@gmail.com>
2025-04-10 01:04:02 +0000

    Delete EC_IT143_6.3_fwf_s3_eb.sql

--- EC_IT143_6.3_fwf_s3_eb.sql
+++
@@ -1,6 +0,0 @@
--- Step 3 – Ad hoc query to extract first name
-SELECT 
-    ContactName,
-    LEFT(ContactName, CHARINDEX(' ', ContactName + ' ') - 1) AS FirstName
-FROM 
-    dbo.t_w3_schools_customers;
